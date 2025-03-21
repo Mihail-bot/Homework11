58e685e20612ec745c44055df0b8f4b6b065ad91
Miha <shmihail85@yandex.ru>
2025-03-21 22:06:04 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void opredelenieVisokosnosti(int year)
+
+    {
+        if (year > 1584 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)) {
+            System.out.println(year + " год является високосным.");
+        } else {
+            System.out.println(year + " год не яляется високосным.");
+        }
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+        System.out.println("Задание 1");
+        int year = 2024;
+        opredelenieVisokosnosti(year);
+
+    }
+
+
+}
